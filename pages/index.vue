7b6c6621ded0d89bf8441453a45512425a6c5b98
guoshuai <406601626@qq.com>
2024-03-24 17:42:18 +0800

    测试

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,11 @@
 </script>
 
 <template>
-<div>this is index page</div>
+<div class="index-container">this is index page</div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.index-container{
+  background: $primary;
+}
 </style>
